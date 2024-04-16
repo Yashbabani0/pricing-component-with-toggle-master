7d454b147cc48ebc1b6c3fed643a1e0ee369d6dc
Yashbabani0 <yashbabani09@gmail.com>
2024-04-17 03:41:04 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Background from "./components/Background";
 import Header from "./components/Header";
+import Cards from "./components/Cards";
 
 function App() {
   return (
     <div className="w-full h-screen flex align-middle justify-center text-center relative">
       <Background />
       <Header />
+      <Cards heading="Basic" price="10$" storage="1" users="10" send="10" />
     </div>
   );
 }
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
-function Cards() {
+function Cards(props) {
   return (
-    <div>
-      <h5></h5> 
+    <div className="">
+      <h5>{props.heading}</h5>
+      <h4>{props.price}</h4>
+      <b>{props.storage}</b>
+      <b>{props.users}</b>
+      <b>{props.send}</b>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
